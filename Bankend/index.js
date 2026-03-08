3ac54b663358f8431f315bea9d98e019bbd2abf3
thegod71 <1011gauravsingh@gmail.com>
2026-03-08 06:12:54 +0530

    all backend logic completed

--- Bankend/index.js
+++ Bankend/index.js
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config({});
 import userRoute from "./routes/user.route.js";
-
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -23,6 +25,9 @@ const PORT = process.env.PORT || 3000;
 
 // api
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.listen(PORT, () => {
   connectDB();
